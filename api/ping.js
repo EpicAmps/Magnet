1c2767bfe8848cc3b1d05dc8ad6fd66afcda5fa4
Epic Amps <53057421+EpicAmps@users.noreply.github.com>
2025-07-30 17:30:39 -0700

    Delete api/ping.js

--- api/ping.js
+++
@@ -1,7 +0,0 @@
-export async function notifyFridges(data) {
-  // For MVP, just log it
-  console.log('New note received:', data);
-  
-  // Later you could add webhook notifications, WebSocket updates, etc.
-  return { success: true };
-}
